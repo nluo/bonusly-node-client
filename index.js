3bef9638869ce5bc404d62ca719cf85ec92728da
Nick Luo <nick.luo@me.com>
2015-08-30 22:55:48 +1000

    Work in progress

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var request = require('request'),
     host = 'https://bonus.ly/api/v1',
-    errors = require('./lib/errors');
-
+    errors = require('./lib/errors'),
+    kgo = require('kgo');
 
 function BonuslyClient(token) {
     if (!token) {
@@ -12,18 +12,18 @@ function BonuslyClient(token) {
     this.accessToken = token;
 };
 
-BonuslyClient.prototype.getBonuses = function (callback) {
-
+BonuslyClient.prototype.getBonuses = function(callback) {
     var client = this;
-
-    client.sendGetRequest('bonuses', null, function(error, response, body){
-        if (error) {
-            return callback(error);
-        }
-        console.log('response is ', response, 'body is ', body);
-        body = JSON.parse(body);
-
-        client.parseResponse(response, body, callback);
+    kgo
+    ({
+        method: 'bonuses'
+    })
+    ('request', ['method'], client.sendGetRequest.bind(client))
+    ('process', ['request'], client.parseResponse)
+    (['process'], function(result){
+        callback(null, result)
+    }).on('error', function(error){
+        callback(error);
     });
 };
 
@@ -48,46 +48,57 @@ BonuslyClient.prototype.giveBounus = function(receiverEmail, reason, amount, cal
 };
 
 
-BonuslyClient.prototype.giveBounusToUsers(users, reason, amount, callback) {
+BonuslyClient.prototype.sendPostRequest = function(method, data, callback) {
     var client = this;
 
-};
-
-BonuslyClient.prototype.sendPostRequest = function (method, data, callback) {
-    var client = this,
     url = this.host + '/' + method + '?access_token=' + client.accessToken;
 
-    request.post({url: url, json: true, body: data}, callback);
+    request.post
+    (
+        {
+            url: url,
+            json: true,
+            body: data
+        },
+        callback
+    );
 };
 
 
-BonuslyClient.prototype.sendGetRequest = function (method, data, callback) {
-    var client = this,
-    url = this.host + '/' + method + '?access_token=' + client.accessToken;
-    console.log('url is ', url);
-    request({
-        method: 'GET',
-        uri: url,
-    }, callback);
+BonuslyClient.prototype.sendGetRequest = function(method, callback) {
+    var client = this;
+    url = client.host + '/' + method + '?access_token=' + client.accessToken;
+    request
+    (
+        {
+            method: 'GET',
+            uri: url,
+            json: true
+        },
+        callback
+    );
 };
 
-BonuslyClient.prototype.parseResponse = function(response, body, callback) {
+BonuslyClient.prototype.parseResponse = function(response, callback) {
+    try {
+        body = response.body;
 
-    console.log('body is ', body, 'response is ', response.statusCode);
-    if (response.statusCode == 200 && body.success == true) {
-        return callback(null, body.result);
+        if (response.statusCode == 200 && body.success == true) {
+            return callback(null, body.result);
+        }
+
+        if (response.statusCode == 500) {
+            return callback(new errors.Internal());
+        }
+
+        if (response.statusCode == 400) {
+            return callback(new errors.Malformed());
+        }
+
+        return callback(new Error('Unknown response code / error'));
+    } catch (e) {
+        return callback(e);
     }
-
-
-    if (response.statusCode == 500) {
-        return callback(new errors.Internal());
-    }
-
-    if (response.statusCode == 400) {
-        return callback(new errors.Malformed());
-    }
-
-    callback(new Error('Unknown response code / error'));
 };
 
 module.exports = BonuslyClient;
--- sample.js
+++ sample.js
@@ -2,6 +2,6 @@ var BonuslyClient = require('./'),
     client = new BonuslyClient('6337871281d538be7b8d1df3be0363b2');
 
 
-client.giveBounus('andrew.gallagher@temando.com', 'test test nodejs #mateship', 1.5, function(error, response){
-    console.log('error is ', error, ' response is ', response);
+client.getBonuses(function(){
+    console.log('the responses is ', arguments);
 });
