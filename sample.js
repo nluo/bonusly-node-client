32662c7d189b1d270e91235b63825c2eb72615c0
Nick Luo <nick.luo@me.com>
2015-08-31 10:26:46 +1000

    Removed sample.js

--- sample.js
+++
@@ -1,7 +0,0 @@
-var BonuslyClient = require('./'),
-    client = new BonuslyClient('6337871281d538be7b8d1df3be0363b2');
-
-
-client.getBonusForId('55e00da74010d88f7f000020', function(){
-    console.log('the responses is ', arguments);
-});
